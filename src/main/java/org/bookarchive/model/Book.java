5ac683a61b5b83a1c97368c6dda576caa5632a75
MatthewEarlywine <MatthewEarlywine@gmail.com>
2022-11-07 08:02:24 -0600

    Fleshed out Author and Book models

--- src/main/java/org/bookarchive/model/Book.java
+++ src/main/java/org/bookarchive/model/Book.java
@@ -7,4 +7,36 @@ public class Book {
 	private String author;
 	private String genre;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
 }
