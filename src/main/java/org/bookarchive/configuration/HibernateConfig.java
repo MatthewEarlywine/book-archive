650e0aeea89d09582b18ccbb75e14d391b69f39b
MatthewEarlywine <MatthewEarlywine@gmail.com>
2022-11-21 10:26:56 -0600

    updates

--- src/main/java/org/bookarchive/configuration/HibernateConfig.java
+++ src/main/java/org/bookarchive/configuration/HibernateConfig.java
@@ -10,7 +10,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -40,6 +45,13 @@ public class HibernateConfig {
 		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
 		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
 		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+		Resource initialSchema = new ClassPathResource("schema.sql");
+		Resource initialData = new ClassPathResource("data.sql");
+		DatabasePopulator schemaBuilder = new ResourceDatabasePopulator(initialSchema);
+		DatabasePopulator databasePopulator = new ResourceDatabasePopulator(initialData);
+		DatabasePopulatorUtils.execute(schemaBuilder, dataSource);
+		DatabasePopulatorUtils.execute(databasePopulator, dataSource);
 		return dataSource;
 	}
 
--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,9 +1,10 @@
-DROP SCHEMA IF EXISTS library;
+DROP TABLE bookarchive.book;
+DROP SCHEMA IF EXISTS bookarchive;
+
 CREATE SCHEMA bookarchive AUTHORIZATION sa;
 
-DROP TABLE IF EXISTS bookarchive.book;
 CREATE TABLE bookarchive.book ( 
-   book_id VARCHAR(10) NOT NULL, 
+   book_id INT NOT NULL, 
    book_title VARCHAR(50) NOT NULL,
    book_series VARCHAR(20),
    book_author VARCHAR(20) NOT NULL,
