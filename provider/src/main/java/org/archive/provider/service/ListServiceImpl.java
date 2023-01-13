708e6cfb6e9df474c4a750fa99172930d7c8d5de
MatthewEarlywine <MatthewEarlywine@gmail.com>
2023-01-13 08:04:48 -0600

    Service testing at 100% coverage

--- provider/src/main/java/org/archive/provider/service/ListServiceImpl.java
+++ provider/src/main/java/org/archive/provider/service/ListServiceImpl.java
@@ -1,14 +1,8 @@
 package org.archive.provider.service;
 
 import java.util.List;
-import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.archive.provider.entity.Book;
 import org.archive.provider.repository.BookDao;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,15 +19,7 @@ public class ListServiceImpl implements ListService {
 	public SessionFactory sessionFactory;
 	
 	@Override
-	@SuppressWarnings("unchecked")
 	public List<Book> findAllBooks() {
-//		Session s = sessionFactory.getCurrentSession();		
-//		CriteriaBuilder cb = s.getCriteriaBuilder();		
-//		CriteriaQuery<Book> cq = cb.createQuery(Book.class);		
-//		Root<Book> root = cq.from(Book.class);		
-//		cq.select(root);		
-//		Query query = s.createQuery(cq);		
-//		return query.getResultList();
 		return bookRepo.findAll();
 	}
 
@@ -64,22 +50,9 @@ public class ListServiceImpl implements ListService {
 
 	@Override
 	public boolean doesBookExist(Book book) {
-		
-//		Query query = sessionFactory.openSession().createQuery("FROM Book b WHERE b.title = :title and b.author = :author and b.illustrator = :illustrator");
-//		query.setParameter("title", book.getTitle());
-//		query.setParameter("author", book.getAuthor());
-//		query.setParameter("illustrator", book.getIllustrator());
-//		
-//		List<?> pList = query.getResultList();
-//		
-//		if(!pList.isEmpty()) {
-//			return true;
-//		}
-//		return false;
-		
-		String title = book.getTitle();
-		String author = book.getAuthor();
-		String illustrator = book.getIllustrator();
+		book.getTitle();
+		book.getAuthor();
+		book.getIllustrator();
 		
 		if(bookRepo.findByTitleAndAuthorAndIllustrator(book.getTitle(), book.getAuthor(), book.getIllustrator()) != null) {
 			return true;
