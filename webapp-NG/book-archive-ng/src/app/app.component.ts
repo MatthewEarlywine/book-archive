ceffc0c21f60197a22067684a7f92d845af220fc
MatthewEarlywine <MatthewEarlywine@gmail.com>
2023-02-13 16:23:45 -0600

    added bootstrap font-awesome

--- webapp-NG/book-archive-ng/src/app/app.component.ts
+++ webapp-NG/book-archive-ng/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ba-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <div><h1>{{pageTitle}}</h1></div>
+  `
 })
 export class AppComponent {
-  title = 'book-archive-ng';
+  pageTitle = 'Book Archive';
 }
