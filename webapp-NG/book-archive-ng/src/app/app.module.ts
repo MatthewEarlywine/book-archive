f1d85846934a26eb4b610a55942e63872909d2bc
MatthewEarlywine <MatthewEarlywine@gmail.com>
2023-02-14 07:51:59 -0600

    created book-list and homepage components

--- webapp-NG/book-archive-ng/src/app/app.module.ts
+++ webapp-NG/book-archive-ng/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './books/book-list.component';
+import { HomepageComponent } from './home/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule
