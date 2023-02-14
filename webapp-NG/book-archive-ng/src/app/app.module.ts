5e14c6c4e829aef8aad574afab3f6fb1f13add45
MatthewEarlywine <MatthewEarlywine@gmail.com>
2023-02-14 12:46:12 -0600

    touch-ups

--- webapp-NG/book-archive-ng/src/app/app.module.ts
+++ webapp-NG/book-archive-ng/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BookListComponent } from './books/book-list.component';
+import { BookListComponent } from './book-list/book-list.component';
 import { HomepageComponent } from './home/homepage.component';
 
 @NgModule({
