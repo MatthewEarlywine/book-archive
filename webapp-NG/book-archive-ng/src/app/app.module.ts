2247929bfe7ba4de05156cf65a6090591f1f3eeb
MatthewEarlywine <MatthewEarlywine@gmail.com>
2023-02-15 16:03:09 -0600

    reorganized

--- webapp-NG/book-archive-ng/src/app/app.module.ts
+++ webapp-NG/book-archive-ng/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
-import { LogInComponent } from './home/log-in.component';
-import { LandingComponent } from './home/landing.component';
+import { LogInComponent } from './login/log-in.component';
+import { LandingComponent } from './landing/landing.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
