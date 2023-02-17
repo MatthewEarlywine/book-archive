21841e265f3bda5b043f1a3288e6d04c299a55b3
MatthewEarlywine <MatthewEarlywine@gmail.com>
2023-02-17 12:36:55 -0600

    beginning testing process

--- webapp-NG/book-archive-ng/src/app/app.component.spec.ts
+++ webapp-NG/book-archive-ng/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'book-archive-ng'`, () => {
+  it(`should have as title 'Book Archive'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-archive-ng');
+    expect(app.pageTitle).toEqual('Book Archive');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('book-archive-ng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Book Archive');
   });
 });
