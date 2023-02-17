1076bb25ec7864c314c83e947b6a141711be4fde
MatthewEarlywine <MatthewEarlywine@gmail.com>
2023-02-17 16:40:01 -0600

    touching up tests

--- provider/src/test/java/org/archive/provider/Web_Tests/ListPageTests.java
+++ provider/src/test/java/org/archive/provider/Web_Tests/ListPageTests.java
@@ -255,7 +255,7 @@ public class ListPageTests {
 		
 		submit.click();
 		
-		WebElement listButton = driver.findElement(By.className("BookListButton"));
+		WebElement listButton = driver.findElement(By.name("BookListButton"));
 		
 		listButton.click();
 		
@@ -308,7 +308,7 @@ public class ListPageTests {
 		
 		submit.click();
 		
-		WebElement listButton = driver.findElement(By.className("BookListButton"));
+		WebElement listButton = driver.findElement(By.name("BookListButton"));
 		
 		listButton.click();
 		
@@ -361,7 +361,7 @@ public class ListPageTests {
 		
 		submit.click();
 		
-		WebElement listButton = driver.findElement(By.className("BookListButton"));
+		WebElement listButton = driver.findElement(By.name("BookListButton"));
 		
 		listButton.click();
 		
@@ -449,7 +449,7 @@ public class ListPageTests {
 		
 		submit.click();
 		
-		WebElement listButton = driver.findElement(By.className("BookListButton"));
+		WebElement listButton = driver.findElement(By.name("BookListButton"));
 		
 		listButton.click();
 		
@@ -515,7 +515,7 @@ public class ListPageTests {
 		
 		submit.click();
 		
-		WebElement listButton = driver.findElement(By.className("BookListButton"));
+		WebElement listButton = driver.findElement(By.name("BookListButton"));
 		
 		listButton.click();
 		
@@ -581,7 +581,7 @@ public class ListPageTests {
 		
 		submit.click();
 		
-		WebElement listButton = driver.findElement(By.className("BookListButton"));
+		WebElement listButton = driver.findElement(By.name("BookListButton"));
 		
 		listButton.click();
 		
@@ -649,7 +649,7 @@ public class ListPageTests {
 		
 		submit.click();
 		
-		WebElement listButton = driver.findElement(By.className("BookListButton"));
+		WebElement listButton = driver.findElement(By.name("BookListButton"));
 		
 		listButton.click();
 		
